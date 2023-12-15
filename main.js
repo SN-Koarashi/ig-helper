66de109b395662aefc4e1195010743964fd6f2be
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2023-12-16 02:43:52 +0800

    Fixed fetch wrong username on post

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.12.5
+// @version            2.12.6
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -955,7 +955,7 @@
                 });
 
                 // Add the mark that download is ready
-                var username = $(this).find("header > div:last-child > div:first-child span").first().text();
+                var username = $(this).find("header > div:last-child > div:first-child span a").first().text();
 
                 $(this).attr('data-snig','canDownload');
                 $(this).attr('data-username',username);
