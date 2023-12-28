7001781fe3d7b9ac21ca453cd1d0d9b22e56d97a
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2023-12-29 04:44:35 +0800

    Fixed download button in new layout post

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.12.7
+// @version            2.12.8
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -812,7 +812,7 @@
         if(NoDialog == false){
             var repeat = setInterval(() => {
                 // div.x13ehr01 << (sigle post in top, not floating) >>
-                if($('article[data-snig="canDownload"], section:visible > main > div > div.xdt5ytf > div[data-snig="canDownload"]').length > 0) clearInterval(repeat);
+                if($('article[data-snig="canDownload"], section:visible > main > div > div.xdt5ytf > div[data-snig="canDownload"], div[id^="mount"] > div > div > div.x1n2onr6.x1vjfegm div[data-snig="canDownload"]').length > 0) clearInterval(repeat);
                 createDownloadButton();
             },250);
         }
