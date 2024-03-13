dc3b87c3c734e7a595233921f64c159d584227d3
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2024-03-13 23:13:45 +0800

    Change counter position in the post

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.24.4
+// @version            2.24.5
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -1385,6 +1385,11 @@
                     $mainElement = $('div._aap0[role="presentation"]').parents('div._aamm').parent().parent().parent().parent().parent();
                 }
                 */
+
+                // Has counter?!
+                if($mainElement.find('._aao_ + div.x6s0dn4').length > 0){
+                    $mainElement.find('._aao_ + div.x6s0dn4').css('top', '35px');
+                }
 
                 const $childElement = $mainElement.children("div").children("div");
 
