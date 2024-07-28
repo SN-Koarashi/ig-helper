e6d41c8f2c31165b65ccdedd05c0b4cf7bf0af70
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2024-07-29 00:57:18 +0800

    Update comments

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.27.3
+// @version            2.27.4
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -1285,7 +1285,8 @@
      * onReadyMyDW
      * Create an event entry point for the download button for the post
      *
-     * @param  {Boolean}  NoDialog - Check if it not showing the dialog
+     * @param  {Boolean}  NoDialog    - Check if it not showing the dialog
+     * @param  {?Boolean}  hasReferrer - Check if the source of the previous page is a story page
      * @return {void}
      */
     function onReadyMyDW(NoDialog, hasReferrer){
