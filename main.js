4fe2e17ac9c168b3b2d9af5cf009db1634b4b7bb
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2024-08-02 18:14:17 +0800

    Fixed button doesn't spawn when be liked story in stories page

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.28.7
+// @version            2.28.8
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -217,7 +217,7 @@
                     console.log('isStory');
 
                     if($('body div[id^="mount"] > div > div > div[class]').length >= 2 &&
-                       $('body div[id^="mount"] > div > div > div[class]').last().find('svg > path[d^="M16.792"]').length > 0 &&
+                       $('body div[id^="mount"] > div > div > div[class]').last().find('svg > path[d^="M16.792"], svg > path[d^="M34.6 3.1c-4.5"]').length > 0 &&
                        $('body div[id^="mount"] > div > div > div[class]').last().find('svg > polyline + line').length > 0
                       ){
                         $('.IG_DWSTORY').remove();
