acaab3eb436ca0aed9ffebe3e922170087d202bd
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2024-08-29 23:27:45 +0800

    Remove console.log

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.29.9
+// @version            2.29.9.1
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -1697,7 +1697,6 @@
             .each(function(index){
             // If it is have not download icon
             // class x1iyjqo2 mean user profile pages post list container
-            console.log(index, $(this), !$(this).attr('data-snig') && !$(this).hasClass('x1iyjqo2') && !$(this).children('article')?.hasClass('x1iyjqo2') && $(this).parents('div#scrollview').length === 0);
             if(!$(this).attr('data-snig') && !$(this).hasClass('x1iyjqo2') && !$(this).children('article')?.hasClass('x1iyjqo2') && $(this).parents('div#scrollview').length === 0){
                 console.log("Found post container", $(this));
 
