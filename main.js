d64879357e5f90b9149413307257752d31f93247
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2024-10-16 23:27:45 +0800

    Improved the issue where thumbnail buttons were sometimes not displayed in stories and highlights

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.36.7
+// @version            2.36.8
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -477,6 +477,20 @@
                             }
                         });
                     }
+
+                    $element.find('video').each(function(){
+                        if(!$(this).data('modify-thumbnail')){
+                            logger('(highlight) Added video event listener #modify-thumbnail');
+
+                            $(this).on('timeupdate',function(){
+                                if($element.find('.IG_DWHISTORY_THUMBNAIL').length === 0){
+                                    onHighlightsStoryThumbnail(false);
+                                }
+                            });
+
+                            $(this).attr('data-modify-thumbnail', true);
+                        }
+                    });
                 }
             }
         }
@@ -911,6 +925,20 @@
                             }
                         });
                     }
+
+                    $element.find('video').each(function(){
+                        if(!$(this).data('modify-thumbnail')){
+                            logger('(story) Added video event listener #modify-thumbnail');
+
+                            $(this).on('timeupdate',function(){
+                                if($element.find('.IG_DWSTORY_THUMBNAIL').length === 0){
+                                    onStoryThumbnail(false);
+                                }
+                            });
+
+                            $(this).attr('data-modify-thumbnail', true);
+                        }
+                    });
 
                     onStoryThumbnail(false);
                 }
