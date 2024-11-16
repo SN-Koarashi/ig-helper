6f3ac2af0bd4032744332f1e2d66997e0be22cb8
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2024-11-16 21:27:39 +0800

    Added debug message

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.36.12
+// @version            2.36.13
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -1671,14 +1671,22 @@
             GM_xmlhttpRequest({
                 method: "GET",
                 url: getURL,
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; Pixel 7 XL)Build/RP1A.20845.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/5.0 Chrome/117.0.5938.60 Mobile Safari/537.36 Instagram 307.0.0.34.111',
+                    'X-IG-App-ID': getAppID()
+                },
                 onload: function(response) {
                     try{
                         let obj = JSON.parse(response.response);
                         logger('getPostOwner()',obj);
-                        resolve(obj.data.shortcode_media.owner.username);
+                        if(obj.feedback_message){
+                            alert(`getPostOwner()\n${obj.message}\n${obj.feedback_message}`);
+                        }
+
+                        resolve(obj?.data?.shortcode_media.owner.username);
                     }
                     catch(err){
-                        logger('getPostOwner()','reject',err.message);
+                        logger('getPostOwner()','reject',err.message, response.response);
                         reject(err);
                     }
                 },
