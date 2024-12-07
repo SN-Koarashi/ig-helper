b0ceb637f4d911b76283e8455269e3609516af8a
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2024-12-07 23:46:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.39.2
+// @version            2.39.3
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -2602,10 +2602,11 @@
         const second = date.getSeconds().toString().padStart(2,'0');
 
         var filename = RENAME_FORMAT.toUpperCase();
+        var format_shortcode = shortcode ?? "";
         var replacements = {
             '%USERNAME%': username,
             '%SOURCE_TYPE%': sourceType,
-            '%SHORTCODE%': (shortcode)?shortcode:'',
+            '%SHORTCODE%': format_shortcode,
             '%YEAR%': year,
             '%2-YEAR%': year.substr(-2),
             '%MONTH%': month,
