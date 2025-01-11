21dbc57aacb4a78fb99096d5e89150ec386ad825
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2025-01-12 07:18:07 +0800

    Fixed click to redirect via user avatar not working in homepage

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.41.1
+// @version            2.41.2
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -3585,7 +3585,7 @@
             if(e.which === 3 || e.which === 2){
                 if(location.href === 'https://www.instagram.com/' && USER_SETTING.REDIRECT_CLICK_USER_STORY_PICTURE){
                     e.preventDefault();
-                    if($(this).find('canvas._aarh').length > 0){
+                    if($(this).find('canvas._aarh, canvas + span > img').length > 0){
                         const targetUrl = 'https://www.instagram.com/'+$(this).children('div').last().text();
                         if(e.which === 2){
                             GM_openInTab(targetUrl);
