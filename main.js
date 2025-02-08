3821f39a913cfd09caba6bf12be32228098be930
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2025-02-08 13:24:13 +0800

    Fixed button group has disappeared in home page

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            2.43.1
+// @version            2.43.2
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -2203,7 +2203,7 @@
      */
     function createDownloadButton() {
         // Add download icon per each posts
-        $('article[class], section:visible > main > div > div > div > div > div > hr').map(function (index) {
+        $('article, section:visible > main > div > div > div > div > div > hr').map(function (index) {
             return $(this).is('section:visible > main > div > div > div > div > div > hr') ? $(this).parent().parent().parent().parent()[0] : this;
         }).filter(function () {
             return $(this).height() > 0 && $(this).width() > 0
