24085d18414e5c8a2c93a05c3d56f9cda420d852
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2025-02-19 16:51:56 +0800

    remove webpack

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './entry.js', // 你的入口文件
-    output: {
-        filename: './bundle.js', // 輸出檔案名稱
-        path: path.resolve(__dirname), // 輸出路徑
-    },
-};
