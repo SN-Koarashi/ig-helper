564468eff109c1dd51008fec5dd68f82fd4de7e2
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2025-05-18 00:28:56 +0800

    Fixed script is not working on reels

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // @name:ja            IG助手
 // @name:ko            IG조수
 // @namespace          https://github.snkms.com/
-// @version            3.5.4
+// @version            3.5.5
 // @description        Downloading is possible for both photos and videos from posts, as well as for stories, reels or profile picture.
 // @description:zh-TW  一鍵下載對方 Instagram 貼文中的相片、影片甚至是他們的限時動態、連續短片及大頭貼圖片！
 // @description:zh-CN  一键下载对方 Instagram 帖子中的相片、视频甚至是他们的快拍、Reels及头像图片！
@@ -1547,7 +1547,7 @@
                     }
 
                     // reels scroll has [tabindex] but header not.
-                    $('section > main[role="main"] > div[tabindex]').children('div').each(function () {
+                    $('section > main[role="main"] > div[tabindex], section > main[role="main"] > div[class]').children('div').each(function () {
                         if ($(this).children().length > 0) {
                             if (!$(this).children().find('.IG_REELS').length) {
                                 var $main = $(this);
